b31e89e452e7d53016d95b9ba6cac9eeefe4a752
jan-dryml <jan.dryml@etnetera.cz>
2025-12-29 17:38:40 +0100

    fixup startup

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.2.21"
+    application
 }
 
 sourceSets {
--- src/org/Day01.kt
+++ src/org/Day01.kt
@@ -10,11 +10,11 @@ fun main() {
     }
 
     // Test if implementation meets criteria from the description, like:
-//    check(part1(listOf("test_input")) == 1)
+    check(part1(listOf("test_input")) == 3)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
     val testInput = readInput("Day01_test")
-//    check(part1(testInput) == 3)
+    check(part1(testInput) == 3)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
